ad98d8abdf23a83c20d5888ba12bce2020fb9987
cbfcbf <cbfcbf.byron@gmail.com>
2024-09-03 14:50:44 +0800

    finish wbc and simulation

--- legged_controllers/launch/init.py
+++ legged_controllers/launch/init.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python
+#!/bin/python3
 
 import rospy
 from gazebo_msgs.srv import SetModelConfiguration
+import numpy as np
 
 def set_joint_positions():
-    rospy.init_node('set_joint_positions_node')
+    rospy.init_node('init')
     
     # 等待服务可用
     rospy.wait_for_service('/gazebo/set_model_configuration')
@@ -17,8 +18,8 @@ def set_joint_positions():
         response = set_joint_state(
             model_name='h1_description',  # 替换为你的模型名称
             urdf_param_name='legged_robot_description',
-            joint_names=['left_knee_joint', 'right_knee_joint'],
-            joint_positions=[0.5, 0.5]  # 设置关节的初始位置
+            joint_names=['left_knee_joint', 'right_knee_joint','left_hip_pitch_joint','right_hip_pitch_joint','left_ankle_joint','right_ankle_joint'],
+            joint_positions=np.array([1, 1,-0.5,-0.5,-0.5,-0.5])*1.8  # 设置关节的初始位置
         )
         
         if response.success:
